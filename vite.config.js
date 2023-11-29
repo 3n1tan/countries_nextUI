864eb542b7c66e02dd9f257008e76c23984a3089
Enitan Odupitan <odupitanenitan@gmail.com>
2023-11-29 14:58:06 +0200

    applies config for github page hosting

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import mdx from '@mdx-js/rollup'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/countries_nextUI",
   plugins:[
     { enforce: 'pre', ...mdx() },
     react({ include: /\.(mdx|js|jsx|ts|tsx)$/ }),
