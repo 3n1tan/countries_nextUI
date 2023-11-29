1bcad318df8e816574688f0a91c6ac71aeead549
Enitan Odupitan <odupitanenitan@gmail.com>
2023-11-29 15:21:07 +0200

    more fixes

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import mdx from '@mdx-js/rollup'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/countries_nextUI/",
   plugins:[
     { enforce: 'pre', ...mdx() },
     react({ include: /\.(mdx|js|jsx|ts|tsx)$/ }),
