332ebcdb492ec33f27a32657323db92f7167abd5
Enitan Odupitan <odupitanenitan@gmail.com>
2023-11-29 19:26:46 +0200

    more

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -11,8 +11,8 @@ import {
   NavbarMenu,
   NavbarMenuItem,
   Image,
-  Link
 } from "@nextui-org/react";
+import { Link } from "react-router-dom";
 import { auth, logout } from "../auth/fireBase";
 import { useAuthState } from "react-firebase-hooks/auth";
 
@@ -29,37 +29,37 @@ const Layout = () => {
           </NavbarBrand>
           <NavbarContent className="sm:flex hidden flex-1 self-start justify-center" justify="end">
             <NavbarItem>
-              <Link href="/">Home</Link>
+              <Link to="/">Home</Link>
             </NavbarItem>
             <NavbarItem>
-              <Link href="/countries">Countries</Link>
+              <Link to="/countries">Countries</Link>
             </NavbarItem>
             <NavbarItem>
-              <Link href="/favourites">Favourites</Link>
+              <Link to="/favourites">Favourites</Link>
             </NavbarItem>
             <NavbarItem>
               {user ? (<Link onClick={logout} href="">Logout</Link>) : (<Link href="/login">Login</Link>)}
             </NavbarItem>
             <NavbarItem>
-              <Link href="/register">Register</Link>
+              <Link to="/register">Register</Link>
             </NavbarItem>
           </NavbarContent>
 
           <NavbarMenu className="flex items-center">
             <NavbarMenuItem className="mb-5 mt-5">
-              <Link href="/">Home</Link>
+              <Link to="/">Home</Link>
             </NavbarMenuItem>
             <NavbarMenuItem className="mb-5">
-              <Link href="/countries">Countries</Link>
+              <Link to="/countries">Countries</Link>
             </NavbarMenuItem>
             <NavbarMenuItem className="mb-5">
-              <Link href="/favourites">Favourites</Link>
+              <Link to="/favourites">Favourites</Link>
             </NavbarMenuItem>
             <NavbarMenuItem className="mb-5">
-              <Link href="/register">Register</Link>
+              <Link to="/register">Register</Link>
             </NavbarMenuItem>
             <NavbarMenuItem>
-              {user ? (<Link onClick={logout} href="">Logout</Link>) : (<Link href="/login">Login</Link>)}
+              {user ? (<Link onClick={logout} to="">Logout</Link>) : (<Link to="/login">Login</Link>)}
             </NavbarMenuItem>
           </NavbarMenu>
 
